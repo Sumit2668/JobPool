72773fc3a3bed8aa114046cbadd0954210d173b5
Sumit2668 <cadburyboy2012@gmail.com>
2017-06-09 13:43:25 +0530

    09June

--- App2/App2/Views/FilterSort.xaml.cs
+++ App2/App2/Views/FilterSort.xaml.cs
@@ -16,6 +16,7 @@ namespace App2.Views
         public FilterSort()
         {
             InitializeComponent();
+            NavigationPage.SetHasBackButton(this, true);
         }
         protected override void OnAppearing()
         {
@@ -24,9 +25,10 @@ namespace App2.Views
             {
                 var calcScreenWidth = Application.Current.MainPage.Width;
                 var calcScreenHieght = Application.Current.MainPage.Height;
-                //FSort.HeightRequest= FSort.WidthRequest=FDepartment.HeightRequest =FDepartment.WidthRequest=FSalary.HeightRequest=FSalary.WidthRequest=FEducation.HeightRequest=FEducation.WidthRequest=FIndustry.HeightRequest=FIndustry.WidthRequest=FLocation.HeightRequest=FLocation.WidthRequest=FRole.HeightRequest= calcScreenWidth / 4;
-                Row0.Height = Row1.Height = Row2.Height = Row3.Height = Row4.Height = Row5.Height = Row6.Height = Column1.Width= calcScreenWidth / 4-23;
-                
+               FSort.HeightRequest= FDepartment.HeightRequest =FSalary.HeightRequest=FEducation.HeightRequest=FIndustry.HeightRequest=FLocation.HeightRequest=FRole.HeightRequest= calcScreenWidth/ 8;
+                //Row0.Height = Row1.Height = Row2.Height = Row3.Height = Row4.Height = Row5.Height = Row6.Height =
+                    Column1.Width= calcScreenWidth / 3;
+               
                 //MainGrid.HeightRequest = calcScreenHieght / 2 - 70;
                 //CircleImg.WidthRequest = calcScreenWidth / 3;
                 //CircleImg.HeightRequest = calcScreenWidth / 3;
@@ -35,5 +37,5 @@ namespace App2.Views
 
             }
         }
-    }
+    }   
 }
